b32918f56abf428dd56b671871db0afe09ca187e
GomesRita <gomesrita541@gmail.com>
2025-04-15 12:59:04 -0300

    adiciona primeiro projeto

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.texto}>Lontra Final de Semestre</Text>
+      <Image source={require('./assets/LontraPedindoUmPonto.jpg')} style={styles.image}/>
+      <StatusBar backgroundColor="#bebebe" />
     </View>
   );
 }
@@ -13,8 +14,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image:{
+    width: 250,
+    height: 350,
+  },
+  texto:{
+    fontSize:20,
+    color: '#fff',
+    backgroundColor: '#bebebe',
+    margin: 10,
+    paddingLeft: 12,
+    paddingRight: 12
+  },
+  statusbar:{
+    backgroundColor:'#bebebe'
+  }
 });
+
